dc7c72d60e6e46e08785b89c5fcf54bde14c7c1e
alexanderzg <alexanderzarateg@gmail.com>
2024-12-04 11:48:05 -0800

    Updated db credentials

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'aurora-cluster.cluster-cv8ymsukqhpv.us-east-1.rds.amazonaws.com',
+    DB_HOST : 'database-1-instance-1.clq04kmm0hj2.us-east-2.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'YourSecurePassword123!',
-    DB_DATABASE : 'mydatabase'
+    DB_PWD : 'cpsc-465',
+    DB_DATABASE : 'webappdb'
 });
